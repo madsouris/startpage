d2fffe5d5167390bca9807d5e28960908423f6da
madsouris <service@vannrith.com>
2023-05-08 14:32:35 +0700

    #9 added darkmode

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,25 +1,15 @@
 <template>
     <section class="px-4 md:px-16 py-4">
-        <p class="text-gray-300 text-[0.75rem] font-sans">
+        <p class="text-gray-800 dark:text-white text-[0.75rem] font-sans">
             Use hotkey to go faster. <br />
             Designed by
-            <a
-                class="text-white hover:no-underline transition-all"
-                href="//vannrith.com"
-                >@madsouris</a
-            >
+            <a class="text-gray-800 dark:text-white hover:underline transition-all" href="//vannrith.com">@madsouris</a>
             |
-            <a
-                class="text-white hover:no-underline transition-all"
-                href="//instagram.com/procambodia"
-                >@procambodian</a
-            >
+            <a class="text-gray-800 dark:text-white hover:underline transition-all"
+                href="//instagram.com/procambodia">@procambodian</a>
             ---- Code is available on
-            <a
-                class="text-white hover:no-underline transition-al"
-                href="//github.com/madsouris/startpage"
-                >Github</a
-            >
+            <a class="text-gray-800 dark:text-white hover:underline transition-al"
+                href="//github.com/madsouris/startpage">Github</a>
         </p>
     </section>
 </template>
