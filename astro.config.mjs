d66ab5f0d4efb6efb58159ae630000d6b4ad9327
madsouris <service@vannrith.com>
2024-09-22 16:23:54 +0700

    added pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
+import AstroPWA from '@vite-pwa/astro'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind(), AstroPWA()],
     serviceWorker: true,
+    devToolbar: {
+        enabled: true,
+    },
 })
