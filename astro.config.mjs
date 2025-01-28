5002db206259312110b494d234104da59e03f30a
madsouris <service@vannrith.com>
2025-01-28 16:17:38 +0700

    fix conflict

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import AstroPWA from '@vite-pwa/astro'
-import AstroPWA from '@vite-pwa/astro'
 
 export default defineConfig({
     integrations: [
